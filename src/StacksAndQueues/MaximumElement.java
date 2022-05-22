5d7a302e08d7cc868ead4a989a9ef7053aa07c5e
ivaylost <ivaylosst@gmail.com>
2022-05-22 11:17:23 +0300

    next

--- src/StacksAndQueues/MaximumElement.java
+++ src/StacksAndQueues/MaximumElement.java
@@ -1,25 +1,20 @@
 package StacksAndQueues;
 
 import java.util.ArrayDeque;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class MaximumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayDeque<Integer> stack = new ArrayDeque();
-        int num = Integer.parseInt(sc.nextLine());
+        String command = sc.nextLine();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        for(int i=0; i<num; i++){
-            String command = sc.nextLine();
+        while (!command.equals("3")){
             if(command.equals("2")){
-                stack.pop();
-            } else if(command.equals("3")){
-                System.out.println(Collections.max(stack));
-            } else {
-                String[] commands = command.split("\\s+");
-                stack.push(Integer.parseInt(commands[1]));
+
             }
+
+            command = sc.nextLine();
         }
     }
 }
