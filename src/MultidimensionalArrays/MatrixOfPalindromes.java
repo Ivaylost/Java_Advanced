faf1b8fec86f07af7e694dc3ca58c54a94639ab4
ivaylost <ivaylosst@gmail.com>
2022-05-24 09:11:23 +0300

    next

---
+++ src/MultidimensionalArrays/MatrixOfPalindromes.java
@@ -0,0 +1,33 @@
+package MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixOfPalindromes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int row = input[0];
+        int col = input[1];
+        String[][] matrix = new String[row][col];
+
+        int index = 97;
+        int index1 = 97;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = "" + (char)index + (char)index1 + (char)index;
+                index1++;
+            }
+            index++;
+            index1 = index;
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/StacksAndQueues/MaximumElement.java
+++ src/StacksAndQueues/MaximumElement.java
@@ -1,20 +1,25 @@
 package StacksAndQueues;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class MaximumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque();
+        int num = Integer.parseInt(sc.nextLine());
 
-        while (!command.equals("3")){
+        for(int i=0; i<num; i++){
+            String command = sc.nextLine();
             if(command.equals("2")){
-
+                stack.pop();
+            } else if(command.equals("3")){
+                System.out.println(Collections.max(stack));
+            } else {
+                String[] commands = command.split("\\s+");
+                stack.push(Integer.parseInt(commands[1]));
             }
-
-            command = sc.nextLine();
         }
     }
 }
