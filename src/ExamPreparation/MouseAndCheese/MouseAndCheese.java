8aa5ac65896a41a36981d9c306ab7b90e5acbdeb
ivaylost <ivaylosst@gmail.com>
2022-06-20 08:48:01 +0300

    next

---
+++ src/ExamPreparation/MouseAndCheese/MouseAndCheese.java
@@ -0,0 +1,10 @@
+package ExamPreparation.MouseAndCheese;
+
+import java.util.Scanner;
+
+public class MouseAndCheese {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+    }
+}
