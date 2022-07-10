94fc0969190af3b8eea90d90b67443d976098310
ivaylost <ivaylosst@gmail.com>
2022-07-10 21:15:31 +0300

    next

---
+++ src/restaurant/Product.java
@@ -0,0 +1,30 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        setName(name);
+        setPrice(price);
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
