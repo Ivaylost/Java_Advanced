31008784655c1f82dee4cb34fb8b3927ef0297ec
ivaylost <ivaylosst@gmail.com>
2022-07-15 13:56:21 +0300

    next

--- src/person/Main.java
+++ src/person/Main.java
@@ -1,30 +1,50 @@
 package person;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
+        List<Birthable> birthable = new ArrayList<>();
+        List<Identifiable> identifiable = new ArrayList<>();
 
-        Class[] citizenInterfaces = Citizen.class.getInterfaces();
-        if (Arrays.asList(citizenInterfaces).contains(Birthable.class)
-                && Arrays.asList(citizenInterfaces).contains(Identifiable.class)) {
-            Method[] methods = Birthable.class.getDeclaredMethods();
-            Method[] methods1 = Identifiable.class.getDeclaredMethods();
-            Scanner scanner = new Scanner(System.in);
-            String name = scanner.nextLine();
-            int age = Integer.parseInt(scanner.nextLine());
-            String id = scanner.nextLine();
-            String birthDate = scanner.nextLine();
-            Identifiable identifiable = new Citizen(name,age,id,birthDate);
-            Birthable birthable = new Citizen(name,age,id,birthDate);
-            System.out.println(methods.length);
-            System.out.println(methods[0].getReturnType().getSimpleName());
-            System.out.println(methods1.length);
-            System.out.println(methods1[0].getReturnType().getSimpleName());
+        while (!"End".equals(input)) {
+            String[] tokens = input.split("\\s+");
+
+            switch (tokens[0]) {
+                case "Citizen":
+                    Citizen citizen = new Citizen(tokens[1], Integer.parseInt(tokens[2]), tokens[3], tokens[4]);
+                    birthable.add(citizen);
+                    identifiable.add(citizen);
+                    break;
+                case "Robot":
+                    identifiable.add(new Robot(tokens[2], tokens[1]));
+                    break;
+                case "Pet":
+                    birthable.add(new Pet(tokens[1], tokens[2]));
+                    break;
+            }
+
+            input = scanner.nextLine();
         }
 
+        String date = scanner.nextLine();
+
+        boolean x = true;
+
+        for(Birthable item:birthable){
+            if(item.getBirthDate().contains(date)){
+                System.out.println(item.getBirthDate());
+                x = false;
+            }
+        }
+
+        if (x){
+            System.out.println("<no output>");
+        }
     }
 }
